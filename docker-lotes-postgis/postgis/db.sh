ba7dd196d4180f7c7c544d5d863302fcdc79b065
vanesadeneb <vanessa_diesver@hotmail.com>
2023-01-23 15:41:05 -0700

    feat: actualiza estructura de tabla lotes

--- docker-lotes-postgis/postgis/db.sh
+++ docker-lotes-postgis/postgis/db.sh
@@ -2,11 +2,6 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE TABLE lotes ( 
-  srid       INTEGER NOT NULL PRIMARY KEY, 
-  auth_name  VARCHAR(256), 
-  auth_srid  INTEGER, 
-  srtext     VARCHAR(2048), 
-  proj4text  VARCHAR(2048) 
-);
+  CREATE TABLE lotes (id SERIAL PRIMARY KEY,name VARCHAR(128),geom geometry,area VARCHAR(10));
+  CREATE INDEX lotes_gix ON lotes USING GIST (geom)
 EOSQL
