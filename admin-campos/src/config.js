5f2e44c143a46f12175af2052db9b85ecb286987
vanesadeneb <vanessa_diesver@hotmail.com>
2023-01-25 19:11:23 -0700

    reorganización de código para uso de docker-compose

---
+++ admin-campos/src/config.js
@@ -0,0 +1,9 @@
+export default {
+    api: {
+        host: "localhost:4000"
+    },
+    maps: {
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyBt4syi8lqaQnExCqRbZfDBAkqoLG_D31g"
+    }
+}
